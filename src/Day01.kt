7bb4823288649f4b123864bda84a9174b98f5cf0
Martin Kuben <mkuben@purestorage.com>
2021-12-02 15:19:56 +0100

    Day 1 + 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var count = 0
+        for (i in 1 until input.size) {
+            if (input[i] > input[i - 1]) count++
+        }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var count = 0
+        for (i in 3 until input.size) {
+            if (input[i] > input[i - 3]) count++
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = listOf(
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263,
+    )
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt()}
+
+    println(part1(testInput))
+    println(part2(testInput))
     println(part1(input))
     println(part2(input))
 }
--- src/Day02.kt
+++ src/Day02.kt
@@ -1,2 +1,49 @@
-class Day02 {
+enum class Direction {
+    FORWARD,
+    UP,
+    DOWN
+}
+
+data class Command(val direction: Direction, val value: Int)
+
+fun main() {
+    fun part1(input: List<Command>): Int {
+        var horPos = 0
+        var depth = 0
+        for (cmd in input) {
+            when (cmd.direction) {
+                Direction.FORWARD -> horPos += cmd.value
+                Direction.DOWN -> depth += cmd.value
+                Direction.UP -> depth -= cmd.value
+            }
+        }
+        return horPos * depth
+    }
+
+    fun part2(input: List<Command>): Int {
+        var horPos = 0
+        var depth = 0
+        var aim = 0
+        for (cmd in input) {
+            when (cmd.direction) {
+                Direction.FORWARD -> {
+                    horPos += cmd.value
+                    depth += aim * cmd.value
+                }
+                Direction.DOWN -> aim += cmd.value
+                Direction.UP -> aim -= cmd.value
+            }
+        }
+        return horPos * depth
+    }
+
+    fun parseInputLine(line: String): Command {
+        val (dir, v) = line.split(" ", limit = 2)
+        return Command(Direction.valueOf(dir.uppercase()), v.toInt())
+    }
+
+    val input = readInput("Day02").map { parseInputLine(it) }
+
+    println(part1(input))
+    println(part2(input))
 }
